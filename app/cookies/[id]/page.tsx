9351f0f92d1d0baf17255b7154f65f2670374855
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-17 18:54:36 +0100

    add breadcrumbs

--- app/cookies/[id]/page.tsx
+++ app/cookies/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { cookies } from "../../lib/placeholder-data";
 import Image from "next/image";
+import Breadcrumbs from "@/app/ui/breadcrumbs";
+
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
@@ -11,23 +13,35 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 
   return (
     <div className="w-full min-h-screen flex justify-center">
-      <div className="flex flex-col w-full max-w-6xl p-4 md:flex-row md:h-screen">
-        {/* Image container - full width on mobile, left side on desktop */}
-        <div className="flex items-start justify-center w-full mb-6 md:mb-0 md:w-1/3 md:h-full md:pt-8">
-          <Image
-            src={cookie.image_url}
-            alt={cookie.name}
-            width="400"
-            height="400"
-            className="max-w-full max-h-full object-contain rounded"
-          />
-        </div>
+      <div className=" p-4 max-w-6xl md:h-screen ">
+        <Breadcrumbs
+          breadcrumbs={[
+            { label: "Cookies", href: "/cookies" },
+            { label: cookie.name, href: `/cookies/${cookie.id}`, active: true },
+          ]}
+        />
+        <div className="flex flex-col w-full md:flex-row ">
+          {/* Image container - full width on mobile, left side on desktop */}
+          <div className="flex items-start justify-center w-full mb-6 md:mb-0 md:w-1/3 md:h-full">
+            <Image
+              src={cookie.image_url}
+              alt={cookie.name}
+              width="400"
+              height="400"
+              className="max-w-full max-h-full object-contain rounded"
+            />
+          </div>
 
-        {/* Text content - below image on mobile, right side on desktop */}
-        <main className="w-full md:w-2/3 md:p-8 md:flex md:flex-col md:justify-start">
-          <h1 className="text-2xl font-bold mb-4 md:text-3xl">{cookie.name}</h1>
-          <p className="text-gray-700 leading-relaxed">{cookie.description}</p>
-        </main>
+          {/* Text content - below image on mobile, right side on desktop */}
+          <main className="w-full md:w-2/3 md:p-8 md:flex md:flex-col md:justify-start">
+            <h1 className="text-2xl font-bold mb-4 md:text-3xl text-amber-900">
+              {cookie.name}
+            </h1>
+            <p className="text-gray-700 leading-relaxed">
+              {cookie.description}
+            </p>
+          </main>
+        </div>
       </div>
     </div>
   );
---
+++ app/ui/breadcrumbs.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+interface Breadcrumb {
+  label: string;
+  href: string;
+  active?: boolean;
+}
+
+export default function Breadcrumbs({
+  breadcrumbs,
+}: {
+  breadcrumbs: Breadcrumb[];
+}) {
+  return (
+    <nav aria-label="Breadcrumb" className="block mb-8">
+      <ol className="flex ">
+        {breadcrumbs.map((breadcrumb, index) => (
+          <li
+            key={breadcrumb.href}
+            aria-current={breadcrumb.active}
+            className={breadcrumb.active ? "text-gray-900" : "text-gray-500"}
+          >
+            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+            {index < breadcrumbs.length - 1 ? (
+              <span className="mx-3 inline-block">/</span>
+            ) : null}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
