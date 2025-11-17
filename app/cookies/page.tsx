0dd6efb264e42430607fb40e5c5dfe2e2a2652a2
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-17 17:57:29 +0100

    improve img components

--- app/cookies/page.tsx
+++ app/cookies/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { cookies } from "../lib/placeholder-data";
 
 export default function Page() {
@@ -11,10 +12,12 @@ export default function Page() {
             className="rounded overflow-hidden bg-orange-100 cursor-pointer hover:bg-orange-200 transition-colors duration-500"
           >
             <div className="relative w-full pb-[100%] overflow-hidden">
-              <img
+              <Image
                 src={cookie.image_url}
                 alt={cookie.name}
-                className="absolute inset-0 w-full h-full object-cover transition-all duration-300 ease-in-out hover:scale-110"
+                fill
+                sizes="(max-width: 640px) 50vw, (max-width: 768px) 33vw, (max-width: 1024px) 25vw, (max-width: 1280px) 20vw, 16vw"
+                className="object-cover transition-all duration-300 ease-in-out hover:scale-110"
               />
             </div>
             <div className="p-2">
