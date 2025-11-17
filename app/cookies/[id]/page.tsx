036d765d6dba3d1a95ab2515e1270d8f75c77ce9
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-17 18:13:10 +0100

    add detail page

---
+++ app/cookies/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { cookies } from "../../lib/placeholder-data";
+import Image from "next/image";
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const cookie = cookies.find((cookie) => cookie.id === id);
+
+  if (!cookie) {
+    return <div className="p-4">Cookie not found</div>;
+  }
+
+  return (
+    <div className="p-4 flex flex-row">
+      <div className="w-64 h-64 mb-4">
+        <Image
+          src={cookie.image_url}
+          alt={cookie.name}
+          width="400"
+          height="400"
+          className="rounded"
+        />
+      </div>
+      <h1 className="text-2xl font-bold mb-4">{cookie.name}</h1>
+    </div>
+  );
+}
--- app/cookies/page.tsx
+++ app/cookies/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { cookies } from "../lib/placeholder-data";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -7,23 +8,27 @@ export default function Page() {
       <h1 className="text-2xl font-bold mb-4">Cookies List</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {cookies.map((cookie) => (
-          <div
-            key={cookie.id}
-            className="rounded overflow-hidden bg-orange-100 cursor-pointer hover:bg-orange-200 transition-colors duration-500"
-          >
-            <div className="relative w-full pb-[100%] overflow-hidden">
-              <Image
-                src={cookie.image_url}
-                alt={cookie.name}
-                fill
-                sizes="(max-width: 640px) 50vw, (max-width: 768px) 33vw, (max-width: 1024px) 25vw, (max-width: 1280px) 20vw, 16vw"
-                className="object-cover transition-all duration-300 ease-in-out hover:scale-110"
-              />
+          <Link key={cookie.id} href={`/cookies/${cookie.id}`}>
+            <div
+              key={cookie.id}
+              className="rounded overflow-hidden bg-orange-100 cursor-pointer hover:bg-orange-200 transition-colors duration-500"
+            >
+              <div className="relative w-full pb-[100%] overflow-hidden">
+                <Image
+                  src={cookie.image_url}
+                  alt={cookie.name}
+                  fill
+                  sizes="(max-width: 640px) 50vw, (max-width: 768px) 33vw, (max-width: 1024px) 25vw, (max-width: 1280px) 20vw, 16vw"
+                  className="object-cover transition-all duration-300 ease-in-out hover:scale-110"
+                />
+              </div>
+              <div className="p-2">
+                <h2 className="font-semibold text-sm truncate">
+                  {cookie.name}
+                </h2>
+              </div>
             </div>
-            <div className="p-2">
-              <h2 className="font-semibold text-sm truncate">{cookie.name}</h2>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
