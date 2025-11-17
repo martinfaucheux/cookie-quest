099da874222e7ad2df0285d01d89474751ede6c3
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-17 18:39:52 +0100

    change banner color

--- app/cookies/layout.tsx
+++ app/cookies/layout.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="bg-amber-600 text-2xl text-center text-white p-4 font-bold">
+      <div className="bg-[#f59e0b] text-2xl text-center text-white p-4 font-bold">
         <Link href="/cookies">🍪 Cookie Quest</Link>
       </div>
       <main className="grow">{children}</main>
