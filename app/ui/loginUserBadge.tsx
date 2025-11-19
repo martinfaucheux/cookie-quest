6ece32e93ca0cc1fb3b9b3a0ef8a2e065c6c1b73
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-19 15:19:56 +0100

    add user dropdown component

--- app/ui/loginUserBadge.tsx
+++ app/ui/loginUserBadge.tsx
@@ -3,20 +3,25 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
+import { UserDropdown } from "./userDropdown";
 
 export const LoginUserBadge = () => {
   const { data: session } = useSession();
 
   return session && session.user ? (
-    <div className="flex flex-row items-center gap-2">
-      <Image
-        src={session.user.image || "/default-avatar.png"}
-        alt="User Avatar"
-        width={32}
-        height={32}
-        className="rounded-full border-2 border-white"
-      />
-      <span className="text-white">{session.user.name}</span>
+    <div className="relative group">
+      <div className="flex flex-row items-center gap-2 cursor-pointer">
+        <Image
+          src={session.user.image || "/default-avatar.png"}
+          alt="User Avatar"
+          width={32}
+          height={32}
+          className="rounded-full border-2 border-white"
+        />
+        <span className="text-white">{session.user.name}</span>
+      </div>
+
+      <UserDropdown user={session.user} />
     </div>
   ) : (
     <Link href="/login">
