b7235bb7bc8b4b3dad9eb0ea8d7d91570e3e4335
Martin Faucheux <martin.faucheux@gmail.com>
2025-11-19 18:27:47 +0100

    customize modal

--- app/ui/modal.tsx
+++ app/ui/modal.tsx
@@ -7,38 +7,43 @@ const Modal = () => {
   const handleModal = () => {
     setModal(!openModal);
   };
+  const handleClose = () => {
+    setModal(false);
+  };
   return (
     <div>
       <button
         type="button"
-        className="h-10 px-4 font-medium text-sm rounded-md text-white bg-gray-900"
+        className="h-10 px-4 font-medium text-sm rounded-md text-white bg-gray-900 cursor-pointer"
         onClick={handleModal}
       >
         Open Modal
       </button>
       {openModal && (
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
-          <div className="max-w-[460px] bg-white shadow-lg py-2 rounded-md">
-            <h2 className="text-sm font-medium text-gray-900 border-b border-gray-300 py-3 px-4 mb-4">
-              This is my modal.
-            </h2>
+          <div className="max-w-[460px] bg-white shadow-lg py-2 rounded-md border border-gray-300">
+            <h1 className="text-xl font-bold  text-gray-900 border-b border-gray-300 py-3 px-4 mb-4">
+              Add a new Cookie
+            </h1>
             <div className="px-4 pb-4">
               <p className="text-sm font-medium text-gray-700">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et
-                quod quis eaque aliquam necessitatibus vel eligendi laboriosam
-                optio quisquam sunt.
+                Add a new entry to your Cookiepedia!
               </p>
             </div>
-            <div className="border-t border-gray-300 flex justify-between items-center px-4 pt-2">
-              <div className="text-sm font-medium text-gray-700">
-                Example Content
-              </div>
+            <div className="flex justify-end items-center px-4 gap-2">
               <button
                 type="button"
-                className="h-8 px-2 text-sm rounded-md bg-gray-700 text-white"
-                onClick={handleModal}
+                className="h-8 px-2 text-sm rounded-md text-black bg-orange-300 cursor-pointer"
+                onClick={handleClose}
               >
                 Close
+              </button>
+              <button
+                type="button"
+                className="h-8 px-2 text-sm rounded-md border border-orange-900 text-orange-900 cursor-pointer"
+                onClick={handleModal}
+              >
+                Do stuff
               </button>
             </div>
           </div>
